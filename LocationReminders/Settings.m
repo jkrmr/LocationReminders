4b440ac41451771d490596278c5f03f98ee736d3
Jake Romer <jkrmr.io@gmail.com>
2017-05-02 16:09:00 -0700

    Version Settings.m because oh come on

---
+++ LocationReminders/Settings.m
@@ -0,0 +1,23 @@
+//
+//  Settings.m
+//  LocationReminders
+//
+//  Created by Jake Romer on 5/1/17.
+//  Copyright © 2017 Jake Romer. All rights reserved.
+//
+
+#import "Settings.h"
+
+@implementation Settings
++ (NSString *)parseAppId {
+  return @"jkrmr-parse-server";
+}
+
++ (NSString *)parseClientKey {
+  return @"hzr7yH2VNwnyLh74Jz3pT";
+}
+
++ (NSString *)parseServerUrl {
+  return @"https://salty-chamber-67734.herokuapp.com/parse";
+}
+@end
