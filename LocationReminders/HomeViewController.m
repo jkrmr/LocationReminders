f0383a571c1a3cc5292be6a559c64b0408978ae1
Jake Romer <jkrmr.io@gmail.com>
2017-05-03 11:01:32 -0700

    Make the trampoline transparent

--- LocationReminders/HomeViewController.m
+++ LocationReminders/HomeViewController.m
@@ -199,12 +199,13 @@
 
 - (MKOverlayRenderer *)mapView:(MKMapView *)mapView
             rendererForOverlay:(id<MKOverlay>)overlay {
-  
+
   MKCircleRenderer *renderer;
   renderer = [[MKCircleRenderer alloc] initWithCircle:overlay];
   renderer.strokeColor = [UIColor blueColor];
   renderer.fillColor = [UIColor redColor];
-  
+  renderer.alpha = 0.25;
+
   return renderer;
 }
 
